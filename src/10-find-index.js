90660e6d80f0d44d7ea4940c697b7f0edacd0281
sergitdev <itrjnextysq@gmail.com>
2021-04-13 01:49:59 +0300

    solved 10

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -12,11 +12,18 @@
  *
  */
 function findIndex(array, value) {
-  let res = 0;
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === value) {
-      res = i;
-      break;
+  let res;
+  let first = 0;
+  let last = array.length - 1;
+  while (first <= last) {
+    const center = Math.floor((first + last) / 2);
+    if (array[center] === value) {
+      res = center;
+    }
+    if (array[center] > value) {
+      last = center - 1;
+    } else {
+      first = center + 1;
     }
   }
   return res;
